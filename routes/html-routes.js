c855926f2fc1edab98ff29575edf434c61864e2d
Timothy Smith <timothytsmith8@gmail.com>
2021-02-07 11:30:52 -0800

    exported my router variablle in html-routes.js file. File should be complete.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,10 +4,11 @@ const path = require("path");
 const router = require("express").router()
 
 router.get("/notes", (req, res) =>{
-res.sendFile(path.join(__dirname, "notes.html"));
+res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 router.get("*", (req, res) =>{
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
+module.exports = router;
